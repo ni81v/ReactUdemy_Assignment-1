99573140a04915f6ff890992cb27dc11b3627da1
Nikolai <nikolai.varichev@gmail.com>
2020-03-25 20:59:24 +0200

    check

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 'use strict';
 
 
-var money = prompt("What is your budget for a month?");
-var time = prompt("Enter a date in a format YYYY-MM-DD");
-var appData = {
+let money = prompt("What is your budget for a month?"),
+    time = prompt("Enter a date in a format YYYY-MM-DD");
+
+let appData = {
     budget: money,
     timeData: time,
     expenses: {},
@@ -12,9 +13,9 @@ var appData = {
     savings: false
 };
 
-var bidingCost = prompt("Enter biding cost");
-var howMuch = prompt("How much?");
-var daysInMonth = 30;
+let bidingCost = prompt("Enter biding cost"),
+    howMuch = prompt("How much?"),
+    daysInMonth = 30;
 
-appData.expenses.bidingCost = howMuch;
-alert(money / daysInMonth);
+appData.expenses[bidingCost] = howMuch;
+alert(appData.budget / daysInMonth);
